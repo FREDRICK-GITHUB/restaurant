bf7599b660b3a1af36e0cf79cc06c64fb5779975
FREDRICK-GITHUB <kamaufredm@gmail.com>
2022-10-25 08:14:03 +0300

    added use of super keyword for relative paths

--- src/lib.rs
+++ src/lib.rs
@@ -21,3 +21,15 @@ pub fn eat_at_restaurant() {
     //relative path
     front_of_house::hosting::add_to_waitlist();
 }
+
+fn deliver_order() {}
+
+mod back_of_house {
+    fn fix_incorrect_order() {
+        cook_order();
+        //relative path to access deliver_oder() by use of super keyword
+        super::deliver_order();
+    }
+    fn cook_order() {}
+
+}
