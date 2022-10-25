2f8b926feaaa5b5397889a53f811c1df8e9a7971
FREDRICK-GITHUB <kamaufredm@gmail.com>
2022-10-25 08:41:38 +0300

    added public implementations of enum and struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,36 @@
-mod front_of_house {
-    pub mod hosting {
-        
-        pub fn add_to_waitlist() {}
 
-        fn seat_at_table() {}
+mod back_of_house {
+    /* Structs are private by default. A public struct still has private fields unless they are made public*/
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
     }
-  
+    
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast { 
+                toast: String::from(toast), 
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
 
-    mod serving {
-        fn take_order() {}
-        fn serve_order() {}
-        fn take_payment() {}
+    /* If an enum is made public, all its fields are made public by default */
+    pub enum Appetizer {
+        Soup,
+        Salad,
     }
+
 }
 
 pub fn eat_at_restaurant() {
-    //absolute path 
-    crate::front_of_house::hosting::add_to_waitlist();
+    // Order a breakfast in the summer with Rye toast
+    let mut meal = back_of_house::Breakfast::summer("rye");
 
-    //relative path
-    front_of_house::hosting::add_to_waitlist();
-}
+    // Change our mind about what bread we'd like
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please",meal.toast);
 
-fn deliver_order() {}
-
-mod back_of_house {
-    fn fix_incorrect_order() {
-        cook_order();
-        //relative path to access deliver_oder() by use of super keyword
-        super::deliver_order();
-    }
-    fn cook_order() {}
-
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
 }
